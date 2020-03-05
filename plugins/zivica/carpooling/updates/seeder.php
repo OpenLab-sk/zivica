50b83e8b4127885572ba53593751470ec210742f
Matej Glasnák <g.matej02@gmail.com>
2020-03-05 23:12:05 +0100

    fade out animation 2

--- plugins/zivica/carpooling/updates/seeder.php
+++ plugins/zivica/carpooling/updates/seeder.php
@@ -55,5 +55,146 @@ class SeedWebsitesTable extends Seeder
             'cities'        =>  'Bratislava, Senec, Trnava',
             'seats'         =>  '3'
         ]);
+
+        Event::create([
+            'name'       =>  'Skola hrou',
+            'starts_at'  => '2020-03-03 00:00:00',
+            'ends_at'    => '2020-03-03 00:00:00'
+        ]);
+        Event::create([
+            'name'       =>  'Skola nehrou',
+            'starts_at'  => '2020-03-03 00:00:00',
+            'ends_at'    => '2020-03-03 00:00:00'
+        ]);
+        Driver::create([
+            'event_id'  =>  '1',
+            'name'      =>  'Marek',
+            'email'     =>  'email@email.com',
+            'leaves_from'   =>  'Galanta',
+            'leaves_at'     =>  '2020-03-03 00:00:00',
+            'cities'        =>  'Bratislava, Senec, Trnava',
+            'seats'         =>  '3'
+        ]);
+        Driver::create([
+            'event_id'  =>  '1',
+            'name'      =>  'Matej',
+            'email'     =>  'email@email.com',
+            'leaves_from'   =>  'Velky Biel',
+            'leaves_at'     =>  '2020-03-03 00:00:00',
+            'cities'        =>  'Bratislava, Senec, Trnava',
+            'seats'         =>  '3'
+        ]);
+        Driver::create([
+            'event_id'  =>  '2',
+            'name'      =>  'Jozo',
+            'email'     =>  'email@email.com',
+            'leaves_from'   =>  'Galanta',
+            'leaves_at'     =>  '2020-03-03 00:00:00',
+            'cities'        =>  'Bratislava, Senec, Trnava',
+            'seats'         =>  '3'
+        ]);
+        Driver::create([
+            'event_id'  =>  '2',
+            'name'      =>  'Fero',
+            'email'     =>  'email@email.com',
+            'leaves_from'   =>  'Velky Biel',
+            'leaves_at'     =>  '2020-03-03 00:00:00',
+            'cities'        =>  'Bratislava, Senec, Trnava',
+            'seats'         =>  '3'
+        ]);
+
+        Event::create([
+            'name'       =>  'Skola hrou',
+            'starts_at'  => '2020-03-03 00:00:00',
+            'ends_at'    => '2020-03-03 00:00:00'
+        ]);
+        Event::create([
+            'name'       =>  'Skola nehrou',
+            'starts_at'  => '2020-03-03 00:00:00',
+            'ends_at'    => '2020-03-03 00:00:00'
+        ]);
+        Driver::create([
+            'event_id'  =>  '1',
+            'name'      =>  'Marek',
+            'email'     =>  'email@email.com',
+            'leaves_from'   =>  'Galanta',
+            'leaves_at'     =>  '2020-03-03 00:00:00',
+            'cities'        =>  'Bratislava, Senec, Trnava',
+            'seats'         =>  '3'
+        ]);
+        Driver::create([
+            'event_id'  =>  '1',
+            'name'      =>  'Matej',
+            'email'     =>  'email@email.com',
+            'leaves_from'   =>  'Velky Biel',
+            'leaves_at'     =>  '2020-03-03 00:00:00',
+            'cities'        =>  'Bratislava, Senec, Trnava',
+            'seats'         =>  '3'
+        ]);
+        Driver::create([
+            'event_id'  =>  '2',
+            'name'      =>  'Jozo',
+            'email'     =>  'email@email.com',
+            'leaves_from'   =>  'Galanta',
+            'leaves_at'     =>  '2020-03-03 00:00:00',
+            'cities'        =>  'Bratislava, Senec, Trnava',
+            'seats'         =>  '3'
+        ]);
+        Driver::create([
+            'event_id'  =>  '2',
+            'name'      =>  'Fero',
+            'email'     =>  'email@email.com',
+            'leaves_from'   =>  'Velky Biel',
+            'leaves_at'     =>  '2020-03-03 00:00:00',
+            'cities'        =>  'Bratislava, Senec, Trnava',
+            'seats'         =>  '3'
+        ]);
+
+        Event::create([
+            'name'       =>  'Skola hrou',
+            'starts_at'  => '2020-03-03 00:00:00',
+            'ends_at'    => '2020-03-03 00:00:00'
+        ]);
+        Event::create([
+            'name'       =>  'Skola nehrou',
+            'starts_at'  => '2020-03-03 00:00:00',
+            'ends_at'    => '2020-03-03 00:00:00'
+        ]);
+        Driver::create([
+            'event_id'  =>  '1',
+            'name'      =>  'Marek',
+            'email'     =>  'email@email.com',
+            'leaves_from'   =>  'Galanta',
+            'leaves_at'     =>  '2020-03-03 00:00:00',
+            'cities'        =>  'Bratislava, Senec, Trnava',
+            'seats'         =>  '3'
+        ]);
+        Driver::create([
+            'event_id'  =>  '1',
+            'name'      =>  'Matej',
+            'email'     =>  'email@email.com',
+            'leaves_from'   =>  'Velky Biel',
+            'leaves_at'     =>  '2020-03-03 00:00:00',
+            'cities'        =>  'Bratislava, Senec, Trnava',
+            'seats'         =>  '3'
+        ]);
+        Driver::create([
+            'event_id'  =>  '2',
+            'name'      =>  'Jozo',
+            'email'     =>  'email@email.com',
+            'leaves_from'   =>  'Galanta',
+            'leaves_at'     =>  '2020-03-03 00:00:00',
+            'cities'        =>  'Bratislava, Senec, Trnava',
+            'seats'         =>  '3'
+        ]);
+        Driver::create([
+            'event_id'  =>  '2',
+            'name'      =>  'Fero',
+            'email'     =>  'email@email.com',
+            'leaves_from'   =>  'Velky Biel',
+            'leaves_at'     =>  '2020-03-03 00:00:00',
+            'cities'        =>  'Bratislava, Senec, Trnava',
+            'seats'         =>  '3'
+        ]);
     }
 }
--- themes/zivica/assets/javascript/app.js
+++ themes/zivica/assets/javascript/app.js
@@ -1,6 +1,9 @@
 $(() => {
     $('.loading-screen').fadeOut();
 
+    var initialHeaderContainerHeight = $('.header-container').height(),
+        scrolled = false;
+
     $('.single-card.event-card').click(function () {
         window.location.href = window.location.href + 'event/' + $(this).attr('data-event-id');
     })
@@ -9,9 +12,27 @@ $(() => {
         $('.text-wrapper').css('opacity', 1 - (window.pageYOffset / 100));
 
         if ($('.header-container').height() - window.pageYOffset <= 100) {
+            if (scrolled == false) {
+                $('.header-container').append(`
+                    <div class="scrolled-header-container header-container"></div>
+                `)
 
+                $('.scrolled-header-container').css({
+                    height: initialHeaderContainerHeight,
+                    width: '100vw',
+                    position: 'fixed',
+                    left: '0',
+                    top: -(initialHeaderContainerHeight - 100)
+
+                });
+            }
+
+            scrolled = true;
         } else {
-
+            if (scrolled == true) {
+                $('.scrolled-header-container.header-container').remove()
+                scrolled = false;
+            }
         }
     })
 })
