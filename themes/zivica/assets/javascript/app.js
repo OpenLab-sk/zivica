8a62ad613c569c0dd9c8ad7fddb1f86406145a1b
Matej Glasnák <g.matej02@gmail.com>
2020-03-05 22:51:40 +0100

    fade out animation

--- themes/zivica/assets/javascript/app.js
+++ themes/zivica/assets/javascript/app.js
@@ -4,4 +4,14 @@ $(() => {
     $('.single-card.event-card').click(function () {
         window.location.href = window.location.href + 'event/' + $(this).attr('data-event-id');
     })
+
+    $(window).scroll(function () {
+        $('.text-wrapper').css('opacity', 1 - (window.pageYOffset / 100));
+
+        if ($('.header-container').height() - window.pageYOffset <= 100) {
+
+        } else {
+
+        }
+    })
 })
