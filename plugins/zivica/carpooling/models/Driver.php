8175979bdaf48fffc9ea8591b9d63f860ff8d5b8
Matej Glasnák <g.matej02@gmail.com>
2020-03-12 11:49:01 +0100

    Cleaned default model's functions

--- plugins/zivica/carpooling/models/Driver.php
+++ plugins/zivica/carpooling/models/Driver.php
@@ -4,75 +4,23 @@ use Model;
 use Mail;
 use Ramsey\Uuid\Uuid;
 
-/**
- * Driver Model
- */
 class Driver extends Model
 {
     use \October\Rain\Database\Traits\Validation;
 
-    public function beforeCreate()
-    {
-        $this->uuid = Uuid::uuid4()->toString();
-    }
-    public function afterCreate()
-    {
-        // Mail::rawTo('marekguspan4@gmail.com', 'uspesne odoslany mail');
-    }
-
-    /**
-     * @var string The database table used by the model.
-     */
     public $table = 'zivica_carpooling_drivers';
-
-    /**
-     * @var array Guarded fields
-     */
-    protected $guarded = ['*'];
-
-    /**
-     * @var array Fillable fields
-     */
-    protected $fillable = [];
-
-    /**
-     * @var array Validation rules for attributes
-     */
     public $rules = [];
-
-    /**
-     * @var array Attributes to be cast to native types
-     */
-    protected $casts = [];
-
-    /**
-     * @var array Attributes to be cast to JSON
-     */
-    protected $jsonable = [];
-
-    /**
-     * @var array Attributes to be appended to the API representation of the model (ex. toArray())
-     */
-    protected $appends = [];
-
-    /**
-     * @var array Attributes to be removed from the API representation of the model (ex. toArray())
-     */
-    protected $hidden = [];
-
-    /**
-     * @var array Attributes to be cast to Argon (Carbon) instances
-     */
 
     protected $dates = [
         'created_at',
         'updated_at'
     ];
 
-    /**
-     * @var array Relations
-     */
-    public $hasOne = [];
+    public function beforeCreate()
+    {
+        $this->uuid = Uuid::uuid4()->toString();
+    }
+
     public $hasMany = [
         'passengers' => [
             'Zivica\Carpooling\Models\Passenger',
@@ -80,16 +28,11 @@ class Driver extends Model
             'otherKey' => 'id'
         ]
     ];
+
     public $belongsTo = [
         'event' => [
             'Zivica\Carpooling\Models\Event',
             'key' => 'event_id'
         ]
     ];
-    public $belongsToMany = [];
-    public $morphTo = [];
-    public $morphOne = [];
-    public $morphMany = [];
-    public $attachOne = [];
-    public $attachMany = [];
 }
