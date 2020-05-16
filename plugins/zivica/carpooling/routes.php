0b1526b9a66aaa85c86cde48e6c3cdd6e946efa5
Marek Guspan <marekguspan4@gmail.com>
2020-05-16 17:54:58 +0200

    form request validations on backend

--- plugins/zivica/carpooling/routes.php
+++ plugins/zivica/carpooling/routes.php
@@ -4,48 +4,95 @@
     use Zivica\Carpooling\Models\Passenger;
 
     Route::post('/api/add-driver', function () {
-        $data   = \Input::all();
-        $driver = new Driver;
+        $driverData   = \Input::all();
+        $validation = Validator::make($driverData, [
+            'event_id'      =>  'required|numeric',
+            'name'          =>  'required|string',
+            'email'         =>  'required|e-mail',
+            'leaves_from'   =>  'required|string',
+            'leaves_at'     =>  'required|date_format:G:i',
+            'cities'        =>  'required|string',
+            'seats'         =>  'required|numeric|between:1,10'
+        ]);
 
-        $driver->event_id       = $data['event_id'];
-        $driver->name           = $data['name'];
-        $driver->email          = $data['email'];
-        $driver->leaves_from    = $data['leaves_from'];
-        $driver->leaves_at      = $data['leaves_at'];
-        $driver->cities         = $data['cities'];
-        $driver->seats          = $data['seats'];
+        if($validation->passes()) {
+            $driver = new Driver;
 
-        $driver->save();
-        return ['uuid' => $driver->uuid];
+            $driver->event_id       = $driverData['event_id'];
+            $driver->name           = $driverData['name'];
+            $driver->email          = $driverData['email'];
+            $driver->leaves_from    = $driverData['leaves_from'];
+            $driver->leaves_at      = $driverData['leaves_at'];
+            $driver->cities         = $driverData['cities'];
+            $driver->seats          = $driverData['seats'];
+
+            $driver->save();
+
+            return ['uuid' => $driver->uuid];
+        } else if($validation->fails()) {
+            return ['validation' => $validation->messages()];
+        }
     });
 
     Route::post('/api/add-passenger', function () {
         $passengerData  = \Input::all();
-        $passenger      = new Passenger;
 
-        $passenger->name        = $passengerData['name'];
-        $passenger->driver_id   = $passengerData['driver_id'];
-        $passenger->email       = $passengerData['email'];
-        $passenger->phone       = $passengerData['phone'];
-        $passenger->city        = $passengerData['city'];
-        $passenger->note        = $passengerData['note'];
+        $validation = Validator::make($passengerData, [
+            'name'          =>  'required|string',
+            'driver_id'     =>  'required|numeric',
+            'email'         =>  'required|e-mail',
+            'phone'         =>  'required|numeric',
+            'city'          =>  'required|string',
+            'note'          =>  'string'
+        ]);
 
-        $passenger->save();
+        if($validation->passes()) {
+            $passenger      = new Passenger;
+
+            $passenger->name        = $passengerData['name'];
+            $passenger->driver_id   = $passengerData['driver_id'];
+            $passenger->email       = $passengerData['email'];
+            $passenger->phone       = $passengerData['phone'];
+            $passenger->city        = $passengerData['city'];
+            if(isset($passengerData['note'])) {
+                $passenger->note        = $passengerData['note'];
+            }
+
+            $passenger->save();
+
+        } else if($validation->fails()) {
+            return ['validation' => $validation->messages()];
+        }
     });
 
     Route::patch('/api/update-driver/{uuid}', function ($uuid) {
         $data   = \Input::all();
-        $driver = Driver::where('uuid', $uuid)->first();
+        $validation = Validator::make($data, [
+            'event_id'      =>  'required|numeric',
+            'name'          =>  'required|string',
+            'email'         =>  'required|e-mail',
+            'leaves_from'   =>  'required|string',
+            'leaves_at'     =>  'required|date_format:G:i',
+            'cities'        =>  'required|string',
+            'seats'         =>  'required|numeric|between:1,10'
+        ]);
 
-        $driver->event_id       = $data['event_id'];
-        $driver->name           = $data['name'];
-        $driver->email          = $data['email'];
-        $driver->leaves_from    = $data['leaves_from'];
-        $driver->leaves_at      = $data['leaves_at'];
-        $driver->cities         = $data['cities'];
-        $driver->seats          = $data['seats'];
+        if($validation->passes()) {
+            $driver = Driver::where('uuid', $uuid)->first();
 
-        $driver->save();
+            $driver->event_id       = $data['event_id'];
+            $driver->name           = $data['name'];
+            $driver->email          = $data['email'];
+            $driver->leaves_from    = $data['leaves_from'];
+            $driver->leaves_at      = $data['leaves_at'];
+            $driver->cities         = $data['cities'];
+            $driver->seats          = $data['seats'];
+
+            $driver->save();
+
+        } else if($validation->fails()) {
+            return ['validation' => $validation->messages()];
+        }
     });
 
     Route::delete('/api/delete-driver/{uuid}', function ($uuid) {
