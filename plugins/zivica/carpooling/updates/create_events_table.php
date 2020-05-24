8e532997a7fb83678fa50e272b6589eccb14e1b9
Marek Guspan <marekguspan4@gmail.com>
2020-05-25 00:18:50 +0200

    changed default event location

--- plugins/zivica/carpooling/updates/create_events_table.php
+++ plugins/zivica/carpooling/updates/create_events_table.php
@@ -14,7 +14,7 @@ class CreateEventsTable extends Migration
             $table->string('name');
             $table->dateTime('starts_at');
             $table->dateTime('ends_at');
-            $table->string('location')->default('Halova 16, Bratislava');
+            $table->string('location')->default('Polomy 20, 962 63 Pliešovce');
             $table->timestamps();
         });
     }
