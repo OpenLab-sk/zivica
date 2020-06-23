591779308560909564196a7cafb82bcf009bcb6b
Marek Guspan <marekguspan4@gmail.com>
2020-06-23 15:51:22 +0200

    reminder set to one day before seminar

--- plugins/zivica/carpooling/models/Driver.php
+++ plugins/zivica/carpooling/models/Driver.php
@@ -41,7 +41,8 @@ class Driver extends Model
 
         $starts_at = Event::where('id', $this->event_id)->value('starts_at');
         $carbonStartsAt = Carbon::createFromFormat('Y-m-d H:i:s', $starts_at);
-        $later = $carbonStartsAt->subDays(2);
+        $later = $carbonStartsAt->subDay();
+        dd($later);
 
         Queue::later($later, 'Zivica\Carpooling\Classes\SendMail@sendReminderToDriver', ['driver' => $this]);
     }
